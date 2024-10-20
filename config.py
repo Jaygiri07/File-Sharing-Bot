5fc3bb5631db39d91dd87fd80eab3b33c6f6b4f1
Rebelstar <145977706+Jaygiri07@users.noreply.github.com>
2024-10-20 15:54:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://j32025026:<db_diTBNy6AvTz
 DB_NAME = os.environ.get("DATABASE_NAME", "FilmoneFileStoreBot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002215819339"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
